7bcb1f976468045fc1a92854d7a2255c9d3b9017
JohnSik <65951253+masin556@users.noreply.github.com>
2022-02-15 22:55:44 +0900

    docs(new) : variable_arithmetic new folder fiis
    Add file variable_arithmetic01

---
+++ C++Train/variable_arithmetic01/variable/ex03.cpp
@@ -0,0 +1,14 @@
+// ++ --
+
+#include <stdio.h>
+
+int main() {
+	int a = 10;
+	
+	printf("원래 a는 %d 였다... \n", a);
+	a++; //a = a + 1;
+	printf("1증가해서 a는 %d 가 되었다..\n", a);
+	a--;
+	printf("1감소해서 a는 %d 가 되었다..\n", a);
+
+}
